ec60ce9fc6716273262dab90b172470b418d0857
Abdallah Mohsen <abdallahmohsen979@gmail.com>
2022-08-09 16:53:03 +0200

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,47 +17,50 @@ use \App\Http\Controllers\UserController;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-//->middleware('auth:sanctum')
+//  Route::post('orders','OrderController@store');
 
+//API For Product
 Route::apiResource('products','App\Http\Controllers\ProductController');
+
+//API For Category
 Route::apiResource('categories','App\Http\Controllers\CategoryController');
 Route::get('mainCategory',['App\Http\Controllers\CategoryController','mainCategory']);
 Route::get('subCategory/{id}',['App\Http\Controllers\CategoryController','subCategory']);
+
+//API For Orders && Offers && Wishlistes
+Route::apiResource('orders','App\Http\Controllers\OrderController');
+Route::apiResource('offeres','App\Http\Controllers\OfferController');
+Route::apiResource('wishlist','App\Http\Controllers\WishlistController');
+
+//API Cart
+Route::apiResource('cart','App\Http\Controllers\CartController');
+Route::get('totalprice/{id}',['App\Http\Controllers\CartController','calcprice']);
+Route::get('totalitem/{id}',['App\Http\Controllers\CartController','totalitem']);
+
+//API For Filter && Search
 Route::get('product/search/{keyword?}',['App\Http\Controllers\FilterControler','SearchByProductName']);
 Route::get('category/filter/{keyword?}',['App\Http\Controllers\FilterControler','filterByCategoryName']);
 Route::get('category/search/{keyword?}',['App\Http\Controllers\FilterControler','searchByCategoryName']);
 Route::post('brand/filter',['App\Http\Controllers\FilterControler','filterByBrandName']);
 
-Route::apiResource('orders','App\Http\Controllers\OrderController');
-Route::apiResource('offeres','App\Http\Controllers\OfferController');
-Route::apiResource('wishlist','App\Http\Controllers\WishlistController');
-Route::apiResource('cart','App\Http\Controllers\CartController');
-Route::get('totalprice/{id}',['App\Http\Controllers\CartController','calcprice']);
-Route::get('totalitem/{id}',['App\Http\Controllers\CartController','totalitem']);
-// Route::post('login',['App\Http\Controllers\Auth\LoginController','login']);
-// Route::post('register',['App\Http\Controllers\Auth\RegisterController','register']);
-//  Route::post('orders','OrderController@store');
-
+//API for User
 Route::put('users/{id}',['App\Http\Controllers\UserController','update']);
 Route::get('users',['App\Http\Controllers\UserController','index']);
+Route::post('changepassword/{id}',['App\Http\Controllers\UserController','ChangePassword']);
+
+
+//API for Reviwes
 Route::get('reviews',['App\Http\Controllers\ReviewController','index']);
 Route::get('reviews/{id}',['App\Http\Controllers\ReviewController','show']);
 Route::post('reviews/{id}',['App\Http\Controllers\ReviewController','store']);
+
+//API for Addresses
 Route::get('countries',['App\Http\Controllers\AddressController','getAllCountries']);
 Route::get('states',['App\Http\Controllers\AddressController','getAllStates']);
 Route::get('cities/{id}',['App\Http\Controllers\AddressController','getAllCities']);
 
 
-
-
-
-// Route::controller(AuthController::class)->group(function () {
-//     Route::post('login', 'login')->name('login');
-
-
+//API Auth
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login')->name('login');
     Route::post('verifyAccount','verifyAccount')->name('verifyAccount');
@@ -66,14 +69,17 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('refresh', 'refresh');
 
 });
-Route::post('changepassword/{id}',['App\Http\Controllers\UserController','ChangePassword']);
 
+
+
+//API For Payment Method
 Route::get('payment',['App\Http\Controllers\PayPalController','payment'])->name("payment");
 Route::get('payment/cancel',['App\Http\Controllers\PayPalController','cancel'])->name("payment.cancel");
 Route::get('payment/success',['App\Http\Controllers\PayPalController','success'])->name("payment.success");
 Route::get('stripe',['App\Http\Controllers\StripePaymentController','stripe']);
 Route::post('stripe',['App\Http\Controllers\StripePaymentController','stripePost'])->name("stripe.post");
 
+//API For Social Login
 Route::get('/login/{provider}', [AuthController::class,'redirectToProvider']);
 Route::get('/login/{provider}/callback', [AuthController::class,'handleProviderCallback']);
 
